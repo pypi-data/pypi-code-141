97530401ce3dae5f451d7360ab3fba27e21802b7
Tom Forbes <tom@tomforb.es>
2022-11-26 06:13:34 +0000

    {"name":"tutorialandexample-zh","version":"2022.11.26.0","file":"tutorialandexample-zh-2022.11.26.0.tar.gz","path":"tutorialandexample-zh/tutorialandexample-zh-2022.11.26.0.tar.gz"}

---
+++ tutorialandexample-zh/tutorialandexample-zh-2022.11.26.0.tar.gz/TutorialandexampleZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tutorialandexample-zh
+https://github.com/apachecn/tutorialandexample-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.26.0"
