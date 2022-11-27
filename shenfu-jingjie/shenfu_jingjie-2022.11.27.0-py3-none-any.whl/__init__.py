015d122af7fb793112620d7422e60d27a42407f6
Tom Forbes <tom@tomforb.es>
2022-11-27 13:38:21 +0000

    {"name":"shenfu-jingjie","version":"2022.11.27.0","file":"shenfu_jingjie-2022.11.27.0-py3-none-any.whl","path":"shenfu-jingjie/shenfu_jingjie-2022.11.27.0-py3-none-any.whl"}

---
+++ shenfu-jingjie/shenfu_jingjie-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenfu-jingjie
+https://github.com/apachecn/shenfu-jingjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
