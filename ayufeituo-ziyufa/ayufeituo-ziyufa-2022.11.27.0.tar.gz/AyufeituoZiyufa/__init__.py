5b500bd43693302b5693215dde3b09ff36ee1a2c
Tom Forbes <tom@tomforb.es>
2022-11-27 12:56:34 +0000

    {"name":"ayufeituo-ziyufa","version":"2022.11.27.0","file":"ayufeituo-ziyufa-2022.11.27.0.tar.gz","path":"ayufeituo-ziyufa/ayufeituo-ziyufa-2022.11.27.0.tar.gz"}

---
+++ ayufeituo-ziyufa/ayufeituo-ziyufa-2022.11.27.0.tar.gz/AyufeituoZiyufa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ayufeituo-ziyufa
+https://github.com/apachecn/ayufeituo-ziyufa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
