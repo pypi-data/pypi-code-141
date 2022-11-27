6d49d07584c23e162e1b17be565c7982989261b0
Tom Forbes <tom@tomforb.es>
2022-11-27 13:27:51 +0000

    {"name":"mailun-chufangde-nengliang-liaoli","version":"2022.11.27.0","file":"mailun_chufangde_nengliang_liaoli-2022.11.27.0-py3-none-any.whl","path":"mailun-chufangde-nengliang-liaoli/mailun_chufangde_nengliang_liaoli-2022.11.27.0-py3-none-any.whl"}

---
+++ mailun-chufangde-nengliang-liaoli/mailun_chufangde_nengliang_liaoli-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mailun-chufangde-nengliang-liaoli
+https://github.com/apachecn/mailun-chufangde-nengliang-liaoli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
