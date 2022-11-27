63af9ec00b3d2d04d1377089069c8949a4f821c7
Tom Forbes <tom@tomforb.es>
2022-11-27 13:32:47 +0000

    {"name":"qidong-shiluode-nengliang-zhiyuan","version":"2022.11.27.0","file":"qidong-shiluode-nengliang-zhiyuan-2022.11.27.0.tar.gz","path":"qidong-shiluode-nengliang-zhiyuan/qidong-shiluode-nengliang-zhiyuan-2022.11.27.0.tar.gz"}

---
+++ qidong-shiluode-nengliang-zhiyuan/qidong-shiluode-nengliang-zhiyuan-2022.11.27.0.tar.gz/QidongShiluodeNengliangZhiyuan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qidong-shiluode-nengliang-zhiyuan
+https://github.com/apachecn/qidong-shiluode-nengliang-zhiyuan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
