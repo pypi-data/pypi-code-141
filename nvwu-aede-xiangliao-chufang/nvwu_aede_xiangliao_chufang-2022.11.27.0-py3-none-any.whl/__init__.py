93ece8ec0e736440c970e9dfece806bf1e45e115
Tom Forbes <tom@tomforb.es>
2022-11-27 13:30:59 +0000

    {"name":"nvwu-aede-xiangliao-chufang","version":"2022.11.27.0","file":"nvwu_aede_xiangliao_chufang-2022.11.27.0-py3-none-any.whl","path":"nvwu-aede-xiangliao-chufang/nvwu_aede_xiangliao_chufang-2022.11.27.0-py3-none-any.whl"}

---
+++ nvwu-aede-xiangliao-chufang/nvwu_aede_xiangliao_chufang-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nvwu-aede-xiangliao-chufang
+https://github.com/apachecn/nvwu-aede-xiangliao-chufang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
