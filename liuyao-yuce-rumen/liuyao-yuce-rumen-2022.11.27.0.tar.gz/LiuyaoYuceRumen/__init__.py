6591cf2381c7c9e1a944b7f86f61aaa097cfd2da
Tom Forbes <tom@tomforb.es>
2022-11-27 13:27:40 +0000

    {"name":"liuyao-yuce-rumen","version":"2022.11.27.0","file":"liuyao-yuce-rumen-2022.11.27.0.tar.gz","path":"liuyao-yuce-rumen/liuyao-yuce-rumen-2022.11.27.0.tar.gz"}

---
+++ liuyao-yuce-rumen/liuyao-yuce-rumen-2022.11.27.0.tar.gz/LiuyaoYuceRumen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuyao-yuce-rumen
+https://github.com/apachecn/liuyao-yuce-rumen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
