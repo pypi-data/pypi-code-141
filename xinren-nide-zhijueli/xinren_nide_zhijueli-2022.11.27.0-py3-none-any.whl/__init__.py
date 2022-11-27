d879dcd9a008a09fd5821277d7951ff91918d02c
Tom Forbes <tom@tomforb.es>
2022-11-27 13:41:25 +0000

    {"name":"xinren-nide-zhijueli","version":"2022.11.27.0","file":"xinren_nide_zhijueli-2022.11.27.0-py3-none-any.whl","path":"xinren-nide-zhijueli/xinren_nide_zhijueli-2022.11.27.0-py3-none-any.whl"}

---
+++ xinren-nide-zhijueli/xinren_nide_zhijueli-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xinren-nide-zhijueli
+https://github.com/apachecn/xinren-nide-zhijueli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
