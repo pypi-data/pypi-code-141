60bfdb7aa3e928c4a31ed943c045e669b657c9ea
Tom Forbes <tom@tomforb.es>
2022-11-27 13:08:52 +0000

    {"name":"jiangui-zhihou","version":"2022.11.27.0","file":"jiangui_zhihou-2022.11.27.0-py3-none-any.whl","path":"jiangui-zhihou/jiangui_zhihou-2022.11.27.0-py3-none-any.whl"}

---
+++ jiangui-zhihou/jiangui_zhihou-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiangui-zhihou
+https://github.com/apachecn/jiangui-zhihou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
