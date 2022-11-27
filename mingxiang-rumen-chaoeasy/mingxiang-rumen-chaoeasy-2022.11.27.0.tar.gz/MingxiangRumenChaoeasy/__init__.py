3e977653e03087f4be5885cf75528aff9f406c3b
Tom Forbes <tom@tomforb.es>
2022-11-27 13:29:10 +0000

    {"name":"mingxiang-rumen-chaoeasy","version":"2022.11.27.0","file":"mingxiang-rumen-chaoeasy-2022.11.27.0.tar.gz","path":"mingxiang-rumen-chaoeasy/mingxiang-rumen-chaoeasy-2022.11.27.0.tar.gz"}

---
+++ mingxiang-rumen-chaoeasy/mingxiang-rumen-chaoeasy-2022.11.27.0.tar.gz/MingxiangRumenChaoeasy/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mingxiang-rumen-chaoeasy
+https://github.com/apachecn/mingxiang-rumen-chaoeasy"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
