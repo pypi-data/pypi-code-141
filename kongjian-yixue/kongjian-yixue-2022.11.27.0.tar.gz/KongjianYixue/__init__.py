8c74c51556ea9ae7536749f9f94f44043ad649c8
Tom Forbes <tom@tomforb.es>
2022-11-27 13:15:19 +0000

    {"name":"kongjian-yixue","version":"2022.11.27.0","file":"kongjian-yixue-2022.11.27.0.tar.gz","path":"kongjian-yixue/kongjian-yixue-2022.11.27.0.tar.gz"}

---
+++ kongjian-yixue/kongjian-yixue-2022.11.27.0.tar.gz/KongjianYixue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kongjian-yixue
+https://github.com/apachecn/kongjian-yixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
