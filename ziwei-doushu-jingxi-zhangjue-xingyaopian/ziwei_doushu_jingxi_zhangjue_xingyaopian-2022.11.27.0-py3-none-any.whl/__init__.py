1a9a04dc6a847a85d334bef72dc28142a88ee96a
Tom Forbes <tom@tomforb.es>
2022-11-27 13:44:27 +0000

    {"name":"ziwei-doushu-jingxi-zhangjue-xingyaopian","version":"2022.11.27.0","file":"ziwei_doushu_jingxi_zhangjue_xingyaopian-2022.11.27.0-py3-none-any.whl","path":"ziwei-doushu-jingxi-zhangjue-xingyaopian/ziwei_doushu_jingxi_zhangjue_xingyaopian-2022.11.27.0-py3-none-any.whl"}

---
+++ ziwei-doushu-jingxi-zhangjue-xingyaopian/ziwei_doushu_jingxi_zhangjue_xingyaopian-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-doushu-jingxi-zhangjue-xingyaopian
+https://github.com/apachecn/ziwei-doushu-jingxi-zhangjue-xingyaopian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
