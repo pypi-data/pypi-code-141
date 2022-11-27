696a24b92bc2348ad6c2cae05c3a6b01019b9479
Tom Forbes <tom@tomforb.es>
2022-11-27 13:09:30 +0000

    {"name":"jiankang-caifu-yuaide-rensheng-mimi","version":"2022.11.27.0","file":"jiankang-caifu-yuaide-rensheng-mimi-2022.11.27.0.tar.gz","path":"jiankang-caifu-yuaide-rensheng-mimi/jiankang-caifu-yuaide-rensheng-mimi-2022.11.27.0.tar.gz"}

---
+++ jiankang-caifu-yuaide-rensheng-mimi/jiankang-caifu-yuaide-rensheng-mimi-2022.11.27.0.tar.gz/JiankangCaifuYuaideRenshengMimi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jiankang-caifu-yuaide-rensheng-mimi
+https://github.com/apachecn/jiankang-caifu-yuaide-rensheng-mimi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
