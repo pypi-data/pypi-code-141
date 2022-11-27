f9b0e6c62352b7de8c335c843cda8cdb160215f1
Tom Forbes <tom@tomforb.es>
2022-11-27 13:15:02 +0000

    {"name":"kexue-zhengshi-nixiangde-huichengzhen","version":"2022.11.27.0","file":"kexue-zhengshi-nixiangde-huichengzhen-2022.11.27.0.tar.gz","path":"kexue-zhengshi-nixiangde-huichengzhen/kexue-zhengshi-nixiangde-huichengzhen-2022.11.27.0.tar.gz"}

---
+++ kexue-zhengshi-nixiangde-huichengzhen/kexue-zhengshi-nixiangde-huichengzhen-2022.11.27.0.tar.gz/KexueZhengshiNixiangdeHuichengzhen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kexue-zhengshi-nixiangde-huichengzhen
+https://github.com/apachecn/kexue-zhengshi-nixiangde-huichengzhen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
