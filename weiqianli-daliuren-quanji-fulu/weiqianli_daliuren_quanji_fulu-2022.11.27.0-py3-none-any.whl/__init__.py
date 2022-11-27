3751932766936cadb605ab07bbd159109b4ee2cb
Tom Forbes <tom@tomforb.es>
2022-11-27 13:40:24 +0000

    {"name":"weiqianli-daliuren-quanji-fulu","version":"2022.11.27.0","file":"weiqianli_daliuren_quanji_fulu-2022.11.27.0-py3-none-any.whl","path":"weiqianli-daliuren-quanji-fulu/weiqianli_daliuren_quanji_fulu-2022.11.27.0-py3-none-any.whl"}

---
+++ weiqianli-daliuren-quanji-fulu/weiqianli_daliuren_quanji_fulu-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weiqianli-daliuren-quanji-fulu
+https://github.com/apachecn/weiqianli-daliuren-quanji-fulu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
