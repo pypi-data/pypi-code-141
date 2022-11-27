0e11848c9260ae961be7156b74feacb5b9446e3f
Tom Forbes <tom@tomforb.es>
2022-11-27 13:48:26 +0000

    {"name":"ziwei-suibi-hengji","version":"2022.11.27.0","file":"ziwei_suibi_hengji-2022.11.27.0-py3-none-any.whl","path":"ziwei-suibi-hengji/ziwei_suibi_hengji-2022.11.27.0-py3-none-any.whl"}

---
+++ ziwei-suibi-hengji/ziwei_suibi_hengji-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ziwei-suibi-hengji
+https://github.com/apachecn/ziwei-suibi-hengji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
