75ab8b235b3a99ca0b5d77431c1c899d9814eb65
Tom Forbes <tom@tomforb.es>
2022-11-27 12:57:49 +0000

    {"name":"bazi-rumen-zhuoyongshen","version":"2022.11.27.0","file":"bazi_rumen_zhuoyongshen-2022.11.27.0-py3-none-any.whl","path":"bazi-rumen-zhuoyongshen/bazi_rumen_zhuoyongshen-2022.11.27.0-py3-none-any.whl"}

---
+++ bazi-rumen-zhuoyongshen/bazi_rumen_zhuoyongshen-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bazi-rumen-zhuoyongshen
+https://github.com/apachecn/bazi-rumen-zhuoyongshen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
