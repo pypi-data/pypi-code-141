51459986445c8c2e85de51d68462f7cd7b4dd00d
Tom Forbes <tom@tomforb.es>
2022-11-27 13:15:39 +0000

    {"name":"kuangye-xiezuo","version":"2022.11.27.0","file":"kuangye_xiezuo-2022.11.27.0-py3-none-any.whl","path":"kuangye-xiezuo/kuangye_xiezuo-2022.11.27.0-py3-none-any.whl"}

---
+++ kuangye-xiezuo/kuangye_xiezuo-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kuangye-xiezuo
+https://github.com/apachecn/kuangye-xiezuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
