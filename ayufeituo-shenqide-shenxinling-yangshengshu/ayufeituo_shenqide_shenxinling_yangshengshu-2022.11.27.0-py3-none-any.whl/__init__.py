77da8a1d6e457314e05e3014dd01f6cae225872e
Tom Forbes <tom@tomforb.es>
2022-11-27 12:54:47 +0000

    {"name":"ayufeituo-shenqide-shenxinling-yangshengshu","version":"2022.11.27.0","file":"ayufeituo_shenqide_shenxinling_yangshengshu-2022.11.27.0-py3-none-any.whl","path":"ayufeituo-shenqide-shenxinling-yangshengshu/ayufeituo_shenqide_shenxinling_yangshengshu-2022.11.27.0-py3-none-any.whl"}

---
+++ ayufeituo-shenqide-shenxinling-yangshengshu/ayufeituo_shenqide_shenxinling_yangshengshu-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ayufeituo-shenqide-shenxinling-yangshengshu
+https://github.com/apachecn/ayufeituo-shenqide-shenxinling-yangshengshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
