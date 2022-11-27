7f12e8aae697e488a571800ae926696f2aced40c
Tom Forbes <tom@tomforb.es>
2022-11-27 13:42:07 +0000

    {"name":"xuexi-qimen-dunjiade-diyibenshu","version":"2022.11.27.0","file":"xuexi-qimen-dunjiade-diyibenshu-2022.11.27.0.tar.gz","path":"xuexi-qimen-dunjiade-diyibenshu/xuexi-qimen-dunjiade-diyibenshu-2022.11.27.0.tar.gz"}

---
+++ xuexi-qimen-dunjiade-diyibenshu/xuexi-qimen-dunjiade-diyibenshu-2022.11.27.0.tar.gz/XuexiQimenDunjiadeDiyibenshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xuexi-qimen-dunjiade-diyibenshu
+https://github.com/apachecn/xuexi-qimen-dunjiade-diyibenshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
