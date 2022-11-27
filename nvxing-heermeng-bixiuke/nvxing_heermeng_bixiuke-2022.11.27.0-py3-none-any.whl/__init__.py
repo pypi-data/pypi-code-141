677724acdf5de4d1812e85cc4ebf314789524e1a
Tom Forbes <tom@tomforb.es>
2022-11-27 13:31:30 +0000

    {"name":"nvxing-heermeng-bixiuke","version":"2022.11.27.0","file":"nvxing_heermeng_bixiuke-2022.11.27.0-py3-none-any.whl","path":"nvxing-heermeng-bixiuke/nvxing_heermeng_bixiuke-2022.11.27.0-py3-none-any.whl"}

---
+++ nvxing-heermeng-bixiuke/nvxing_heermeng_bixiuke-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nvxing-heermeng-bixiuke
+https://github.com/apachecn/nvxing-heermeng-bixiuke"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
