8e5ec9d6a99ef9fc431260486d21a1dbe67bad27
Tom Forbes <tom@tomforb.es>
2022-11-27 12:56:15 +0000

    {"name":"ayufeituo-yuanli","version":"2022.11.27.0","file":"ayufeituo_yuanli-2022.11.27.0-py3-none-any.whl","path":"ayufeituo-yuanli/ayufeituo_yuanli-2022.11.27.0-py3-none-any.whl"}

---
+++ ayufeituo-yuanli/ayufeituo_yuanli-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ayufeituo-yuanli
+https://github.com/apachecn/ayufeituo-yuanli"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
