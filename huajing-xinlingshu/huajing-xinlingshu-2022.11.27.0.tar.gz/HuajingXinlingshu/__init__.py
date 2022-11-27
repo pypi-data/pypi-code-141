02c83309a9c248432ab242a24e4dcac75c9e5c6c
Tom Forbes <tom@tomforb.es>
2022-11-27 13:08:12 +0000

    {"name":"huajing-xinlingshu","version":"2022.11.27.0","file":"huajing-xinlingshu-2022.11.27.0.tar.gz","path":"huajing-xinlingshu/huajing-xinlingshu-2022.11.27.0.tar.gz"}

---
+++ huajing-xinlingshu/huajing-xinlingshu-2022.11.27.0.tar.gz/HuajingXinlingshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huajing-xinlingshu
+https://github.com/apachecn/huajing-xinlingshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
