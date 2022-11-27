5964ca83719900e8573ce299b7c342e52e508c3d
Tom Forbes <tom@tomforb.es>
2022-11-27 13:12:26 +0000

    {"name":"kaiguanren-ziwei-doushu-vol2","version":"2022.11.27.0","file":"kaiguanren_ziwei_doushu_vol2-2022.11.27.0-py3-none-any.whl","path":"kaiguanren-ziwei-doushu-vol2/kaiguanren_ziwei_doushu_vol2-2022.11.27.0-py3-none-any.whl"}

---
+++ kaiguanren-ziwei-doushu-vol2/kaiguanren_ziwei_doushu_vol2-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kaiguanren-ziwei-doushu-vol2
+https://github.com/apachecn/kaiguanren-ziwei-doushu-vol2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
