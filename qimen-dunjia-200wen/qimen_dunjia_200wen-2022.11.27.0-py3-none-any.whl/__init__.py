3f3e2119e298803a8cc4bf76fe502c63079f0bcb
Tom Forbes <tom@tomforb.es>
2022-11-27 13:33:09 +0000

    {"name":"qimen-dunjia-200wen","version":"2022.11.27.0","file":"qimen_dunjia_200wen-2022.11.27.0-py3-none-any.whl","path":"qimen-dunjia-200wen/qimen_dunjia_200wen-2022.11.27.0-py3-none-any.whl"}

---
+++ qimen-dunjia-200wen/qimen_dunjia_200wen-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qimen-dunjia-200wen
+https://github.com/apachecn/qimen-dunjia-200wen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
