46420ee9b7597b9253e7825e1d4771766ed79eed
Tom Forbes <tom@tomforb.es>
2022-11-27 13:19:40 +0000

    {"name":"liuyao-shengua-shizheng-jilu","version":"2022.11.27.0","file":"liuyao_shengua_shizheng_jilu-2022.11.27.0-py3-none-any.whl","path":"liuyao-shengua-shizheng-jilu/liuyao_shengua_shizheng_jilu-2022.11.27.0-py3-none-any.whl"}

---
+++ liuyao-shengua-shizheng-jilu/liuyao_shengua_shizheng_jilu-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""liuyao-shengua-shizheng-jilu
+https://github.com/apachecn/liuyao-shengua-shizheng-jilu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
