3fa0bf4c22ea6ddefe847a08990790901e4842c6
Tom Forbes <tom@tomforb.es>
2022-11-27 13:43:37 +0000

    {"name":"zhaohui-nide-neizai-liliang","version":"2022.11.27.0","file":"zhaohui_nide_neizai_liliang-2022.11.27.0-py3-none-any.whl","path":"zhaohui-nide-neizai-liliang/zhaohui_nide_neizai_liliang-2022.11.27.0-py3-none-any.whl"}

---
+++ zhaohui-nide-neizai-liliang/zhaohui_nide_neizai_liliang-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhaohui-nide-neizai-liliang
+https://github.com/apachecn/zhaohui-nide-neizai-liliang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
