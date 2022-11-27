7ac011ed9706b5f54d52d5f984eca683aebc80d5
Tom Forbes <tom@tomforb.es>
2022-11-27 12:59:18 +0000

    {"name":"bentianjiande-kuaile-zhifu-shengjing","version":"2022.11.27.0","file":"bentianjiande-kuaile-zhifu-shengjing-2022.11.27.0.tar.gz","path":"bentianjiande-kuaile-zhifu-shengjing/bentianjiande-kuaile-zhifu-shengjing-2022.11.27.0.tar.gz"}

---
+++ bentianjiande-kuaile-zhifu-shengjing/bentianjiande-kuaile-zhifu-shengjing-2022.11.27.0.tar.gz/BentianjiandeKuaileZhifuShengjing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bentianjiande-kuaile-zhifu-shengjing
+https://github.com/apachecn/bentianjiande-kuaile-zhifu-shengjing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
