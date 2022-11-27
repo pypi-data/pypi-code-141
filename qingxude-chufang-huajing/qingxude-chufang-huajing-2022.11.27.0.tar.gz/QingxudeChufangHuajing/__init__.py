74f740a58f1caba8f5cecf751fb6ea15641bae9d
Tom Forbes <tom@tomforb.es>
2022-11-27 13:37:07 +0000

    {"name":"qingxude-chufang-huajing","version":"2022.11.27.0","file":"qingxude-chufang-huajing-2022.11.27.0.tar.gz","path":"qingxude-chufang-huajing/qingxude-chufang-huajing-2022.11.27.0.tar.gz"}

---
+++ qingxude-chufang-huajing/qingxude-chufang-huajing-2022.11.27.0.tar.gz/QingxudeChufangHuajing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qingxude-chufang-huajing
+https://github.com/apachecn/qingxude-chufang-huajing"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
