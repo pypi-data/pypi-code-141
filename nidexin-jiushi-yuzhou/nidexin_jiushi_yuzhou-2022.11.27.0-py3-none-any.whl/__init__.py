01d9752a624641c98669a87dbc48c18fbbf1e4a2
Tom Forbes <tom@tomforb.es>
2022-11-27 13:29:53 +0000

    {"name":"nidexin-jiushi-yuzhou","version":"2022.11.27.0","file":"nidexin_jiushi_yuzhou-2022.11.27.0-py3-none-any.whl","path":"nidexin-jiushi-yuzhou/nidexin_jiushi_yuzhou-2022.11.27.0-py3-none-any.whl"}

---
+++ nidexin-jiushi-yuzhou/nidexin_jiushi_yuzhou-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nidexin-jiushi-yuzhou
+https://github.com/apachecn/nidexin-jiushi-yuzhou"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
