c923f2fc80c1d3b0adf071083728a90873cb6686
Tom Forbes <tom@tomforb.es>
2022-11-27 13:30:18 +0000

    {"name":"nishi-zijide-yaoshi","version":"2022.11.27.0","file":"nishi_zijide_yaoshi-2022.11.27.0-py3-none-any.whl","path":"nishi-zijide-yaoshi/nishi_zijide_yaoshi-2022.11.27.0-py3-none-any.whl"}

---
+++ nishi-zijide-yaoshi/nishi_zijide_yaoshi-2022.11.27.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nishi-zijide-yaoshi
+https://github.com/apachecn/nishi-zijide-yaoshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.11.27.0"
